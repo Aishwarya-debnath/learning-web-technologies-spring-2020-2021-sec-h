77b43d302a0e46d93df60dd78f17fff9e572566d
Aishwarya-debnath <aishwarya.debnath25@gmail.com>
2021-04-01 16:11:58 +0600

    task1

---
+++ Final_Lab_Task_01_DB/final task1/view/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Landing page</title>
+</head>
+<body>
+
+	<center>
+		<a href="login.html"> Login</a> | 
+		<a href="reg.html"> Signup</a>  
+
+	</center>
+</body>
+</html>
